6634258f2990e0e077b7828fb42221c98663d495
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-05-12 13:19:53 +0200

    Update BankAccount.cs
    withdraw method added throwing new BankException

--- BankAccount.cs
+++ BankAccount.cs
@@ -2,6 +2,15 @@ namespace Bank_account
 {
     public class BankAccount
     {
-        public double Saldo { get; private set; }
+        public double Saldo { get; private set; }\
+
+        public void Withdraw(double amount)
+        {
+            if (amount > Saldo)
+            {
+                throw new BankException("Not enough money on your account");
+            }
+            Saldo -= amount;
+        }
     }
 }
