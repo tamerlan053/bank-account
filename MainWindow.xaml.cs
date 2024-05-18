c068f9b424a8e3a79fd67526755f27acf4678198
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-05-18 23:59:10 +0200

    Update MainWindow.xaml.cs
    cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bank_account
 {
@@ -25,7 +16,7 @@ namespace Bank_account
 
         private void saldoButton_Click(object sender, RoutedEventArgs e)
         {
-            try 
+            try
             {
                 double amount = Convert.ToDouble(depositTextBox.Text);
                 if (amount < 0)
