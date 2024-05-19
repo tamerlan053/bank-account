b4032af412137fa24dde7511f79bb6652bd555b5
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-05-20 00:42:07 +0200

    Update StartWindow.xaml.cs

--- StartWindow.xaml.cs
+++ StartWindow.xaml.cs
@@ -1,1 +1,15 @@
+using System.Windows;
 
+namespace Bank_account
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
